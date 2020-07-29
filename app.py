50e2391d92588d5f8cfe250dfae5abd4ca0b8e51
Eero Häihälä <eero.haihala@gmail.com>
2020-07-29 08:22:49 +0300

    set up autobuild for both node and flask. Installed new python module for websockets and started on it (flask-socketio)

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_socketio import SocketIO
+
 app = Flask("Quince", static_folder='front/build', static_url_path='/')
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return app.send_static_file('index.html')
 
+@socketio.on("event", namespace="/<note>/")
+def process_note(note):
+    print("Got an event on note '{}'".format(note))
